0702f651e6d5d89fab589da6da711449121871a4
stitchinger <g.silitsch@gmail.com>
2014-05-08 20:01:10 +0200

    Hab den dämlichen Kommentar entfernt

--- src/main/Game.java
+++ src/main/Game.java
@@ -14,7 +14,7 @@ import org.newdawn.slick.*;
 
 public class Game extends BasicGame {
 
-    // am arsch
+    
 	// Fenster Einstellungen  +++++++++++++
 	static int width = 1200;
     static int height = 800;
