cd3e2a08479a9ec29bc7e64eab7ef37449e8f303
Georg <g.silitsch@gmail.com>
2014-05-17 23:37:27 +0200

    Temporäre Healthanzeige^^

--- src/main/Gui.java
+++ src/main/Gui.java
@@ -30,7 +30,27 @@ public class Gui {
 	    g.drawString("Velocity Y: " + Game.player.getVelocityY(), posX + 10, posY + 70);
 	    
 	    g.drawString("Score: " + Game.killCount           , posX + this.width - 120, posY + 50);
-	    g.drawString("Health: "+ (Game.player.getHealth()), posX + this.width - 120, posY + 70);	       
+	    g.drawString("Health: "+ (Game.player.getHealth()), posX + this.width - 120, posY + 70);	      
+	    
+	    this.drawTest(g);
+	}
+	
+	public void drawTest(Graphics g){
+		float px = this.posX+ this.width - 120;
+		float py = this.posY + this.height - 120;
+		
+		float healthToDegree =  (Game.player.getHealth() * 3.6f);
+		
+		g.setColor(Color.black);
+		g.fillOval(px, py, 100, 100);
+		
+		g.setColor(new Color(51,179,30));
+		g.fillArc(px, py, 100, 100, 0, healthToDegree);
+		
+		g.setColor(new Color(211,88,50));
+		g.fillOval(this.posX+ this.width - 99, this.posY + this.height - 100, 60, 60);
+		
+		
 	}
 }
 
