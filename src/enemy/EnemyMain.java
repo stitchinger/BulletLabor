ba89459e2be855cfab4d8fa3e5e342b28a18b7b6
Alex <alexander.eggers91@gmail.com>
2014-05-19 18:01:10 +0200

    behavior-methode von enemyMain überarbeitet

--- src/enemy/EnemyMain.java
+++ src/enemy/EnemyMain.java
@@ -11,66 +11,102 @@ public class EnemyMain extends LivingObject{
 	protected boolean unfollow = false;
 	protected int persistent;
 	
-	private float xEnd1;
-	private float xEnd2;
+	protected float xEnd1;
+	protected float xEnd2;
 	
 	public EnemyMain(float x, float y, float xEnd1, float xEnd2) {
         super(x, y);
     }
     
     public void update(int delta){
-       this.behavior(this.aggrorange);
        super.update(delta);
+       
+       
+       this.behavior(this.aggrorange);
     }
     
     public void behavior(int aggrorange){
+    	this.enemyMove();
+    	this.enemyArea();
+    	
+        this.enemyAggro();
+        this.follow();
+        this.followTimer();
+        this.unfollow();
+        
+        this.enemyJump();
+   	}
+    
+    
+    public void enemyJump(){
+    	if(Math.random() < 0.01f && this.isBottomSideCollided()){
+        	this.jump();
+        }
+    }
+    
+    public void enemyMove(){
     	if(direction == "right"){
     		this.moveRight();
         }
         if(direction == "left"){
         	this.moveLeft();
         }
-        
-        //Vorläufige Experimentelle Funktion bzgl. einem Aggro System
-        
-        if(this.getX() < this.xEnd1)
-        	this.moveRight();
-        	
-        if(this.getX() > this.xEnd2)
-        	this.moveLeft();
-	        
-        if((Math.abs(Game.player.getX() - this.getX()) <= aggrorange)){
+    }
+    
+    public void enemyAggro(){
+    	if((Math.abs(Game.player.getX() - this.getX()) <= aggrorange)){
         	if(this.followTimer == 0){
 		        this.follow = true;
 		    }
         }
-        
-        if(this.follow == true){
+    }
+    
+    public void follow(){
+    	if(this.follow == true){
         	this.followTimer++;
 	        if (Game.player.getX() < this.getX())
 	    		this.moveLeft();
 	    	else if (Game.player.getX() > this.getX())
 	    		this.moveRight();
         }
-        
-        if(this.followTimer == this.persistent){
-        	this.follow = false;
-        	this.changeDirection();
-        	this.unfollow = true;
-        }
-        
-        if((this.unfollow == true) && (this.followTimer > 0)){
+    }
+    
+    public void unfollow(){
+    	if((this.unfollow == true) && (this.followTimer > 0)){
         	this.followTimer -= 2;
         	if(this.followTimer == 0)
         		this.unfollow = false;
     	}
-        //Experimentelle Funktion Ende
-        	
-        if(Math.random() < 0.01f && this.isBottomSideCollided()){
-        	this.jump();
-        }
-   	}
+    }
     
+    public void followTimer(){
+    	if(this.followTimer == this.persistent){
+        	this.follow = false;
+        	this.changeDirection();
+        	this.unfollow = true;
+        }
+    }
+    
+    public void enemyArea(){
+    	if(this.getX() < this.xEnd1)
+        	this.moveRight();	
+        if(this.getX() > this.xEnd2)
+        	this.moveLeft();
+    }
+    
+    public void changeDirection(){
+    	if(this.direction == "right"){
+    		this.direction = "left";
+    	}
+    	else if(this.direction == "left"){
+    		this.direction = "right";
+    	}
+    	else {
+    		this.direction = "right";
+    	}
+    }
+    
+    //Setter-Methoden
     public void setAggrorange(int aggrorange){
     	this.aggrorange = aggrorange;
     }
@@ -85,17 +121,5 @@ public class EnemyMain extends LivingObject{
     
     public void setXEnd2(float xEnd2){
     	this.xEnd2 = xEnd2;
-    }
-    
-    public void changeDirection(){
-    	if(this.direction == "right"){
-    		this.direction = "left";
-    	}
-    	else if(this.direction == "left"){
-    		this.direction = "right";
-    	}
-    	else{
-    		this.direction = "right";
-    	}
     }
 }
